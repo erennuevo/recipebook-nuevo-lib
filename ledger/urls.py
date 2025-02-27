fadb0ab869605180b223f7812ff83e4fe0e4b674
Eren <lib.eren09@gmail.com>
2025-02-27 16:32:00 +0800

    Removed .env and pycache files.

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,13 +3,13 @@ URL configuration for the ledger app.
 '''
 
 from django.urls import path
-from .views import index, recipes, recipe1, recipe2
+from .views import recipes, recipe1, recipe2
 
 urlpatterns = [
-    path('', index, name='index'),
     path('recipes/list', recipes, name="recipes"),
     path('recipe/1', recipe1, name="recipe1"),
     path('recipe/2', recipe2, name="recipe1"),
 ]
 
-APP_NAME = "ledger"
+app_name = "ledger"
+
