a4d8deb8b4e9a44ed6a49b717f6d84e0f56a9e3c
Eren <lib.eren09@gmail.com>
2025-03-24 19:57:40 +0800

    Created a form to create new recipes

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,13 +3,14 @@ URL configuration for the ledger app.
 '''
 
 from django.urls import path, include
-from .views import recipe_list, recipe_detail
+from .views import recipe_list, recipe_detail, add_recipe
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('list', recipe_list, name="recipe_list"),
     path('<int:id>/detail', recipe_detail, name="recipe_detail"),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('recipe/add', add_recipe, name="add_recipe")
 ]
 
 app_name = "ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -2,8 +2,9 @@
 Django views that contain the contexts for each page.
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe
+from .forms import RecipeForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -14,7 +15,7 @@ def recipe_list(request):
     recipes = Recipe.objects.all()
 
     ctx = {
-        'recipes': recipes
+        'recipes': recipes,
     }
     return render(request, 'recipe_list.html', ctx)
 
@@ -28,3 +29,17 @@ def recipe_detail(request, id):
         'recipe': Recipe.objects.get(id=id)
     }
     return render(request, 'recipe_detail.html', ctx)
+
+
+@login_required
+def add_recipe(request):
+    form = RecipeForm()
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            recipe = form.save()
+            return redirect('ledger:recipe_detail', id=recipe.id)
+    ctx = { 
+        'form': form 
+    }
+    return render(request, 'recipe_form.html', ctx)
